00274546475b6b9b46a9d50310bcdfb54b655e23
root <root@gloriavm>
2020-01-27 08:17:49 -0500

    Enabling and starting the listener

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ cp ./tcplistener /usr/bin/tcplistener
 cp ./tcplistener.service /etc/systemd/system/tcplistener.service
 
 systemctl daemon-reload
+systemctl enable tcplistener
+systemctl start tcplistener
